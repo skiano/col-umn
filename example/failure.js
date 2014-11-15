590f34c1ec139c395ec0dce69cc771b0dae24903
Greg Skiano <gregoryskiano@gmail.com>
2014-11-15 15:57:00 -0500

    also allow no config

---
+++ example/failure.js
@@ -0,0 +1,24 @@
+
+var makeGrid = require('../lib/');
+
+var grid = makeGrid();
+
+grid
+  .column(9)
+  .nest()
+    .column(5)
+      .nest()
+        .column(2)
+          .nest()
+            .column(1)
+            .column(1)
+        .column(3)
+    .column(3)
+      .nest()
+        .column(2)
+        .column(1)
+    .column(1)
+
+grid.render(function (rootColumn) {
+  console.log(rootColumn)
+});
