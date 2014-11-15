994e5a12ca434c1c7ed974e52656d837865a726f
Greg Skiano <gregoryskiano@gmail.com>
2014-11-15 16:54:11 -0500

    fixing bug with multiple nests

--- example/overflow.js
+++ example/overflow.js
@@ -1,20 +1,49 @@
 
 var makeGrid = require('../lib/');
 
+function testRender (rootColumn) {
+  console.log('-------------------------------------------')
+  console.log(JSON.stringify(rootColumn, null, 2));
+  console.log('-------------------------------------------')
+}
+
 var grid = makeGrid();
 
+// grid
+//   .column(9)
+//   .nest()
+//     .column(5)
+//       .nest()
+//         .column(2)
+//           .nest()
+//             .column(1)
+//             .column(1)
+//         .column(3)
+//     .column(3)
+//       .nest()
+//         .column(2)
+//         .column(1)
+//     .column(4) // too many !
+
+
 grid
-  .column(9)
+  .column(12)
   .nest()
-    .column(5)
+    .column(6)
+    .column(6)
+  .nest()
+    .column(12)
+  .nest()
+    .column(6)
       .nest()
         .column(2)
-          .nest()
-            .column(1)
-            .column(1)
-        .column(3)
-    .column(3)
+        .column(4)
       .nest()
-        .column(2)
-        .column(1)
-    .column(4) // too many !
+        .column(6)
+  // .nest()
+  //   .column(4)
+  //   .column(8)
+  //   ;
+
+grid.render(testRender);
+
