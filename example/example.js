003ea39869c5deece24b4ad4efe2b9cbbe3ae46d
Greg Skiano <gregoryskiano@gmail.com>
2014-11-15 02:25:25 -0500

    making entry to vallidator cleaner

--- example/example.js
+++ example/example.js
@@ -1,11 +1,13 @@
 
 var makeGrid = require('../lib/');
 
-var grid = makeGrid(9);
+var grid = makeGrid();
 
 grid
+  .column(9)
   .setModule('a')
   .setRule('bottom')
+  .setRule('right')
   .nest()
     .column(3)
       .nest(3)
@@ -19,5 +21,27 @@ grid
           .setModule('d')
     .column(6)
       .setModule('e');
+
+// grid.render();
+
+// var grid = makeGrid(9);
+
+// grid
+//   .setModule('a')
+//   .setRule('bottom')
+//   .setRule('right')
+//   .nest()
+//     .column(3)
+//       .nest(3)
+//         .column(2)
+//           .nest()
+//             .column(1)
+//               .setModule('b')
+//             .column(1)
+//               .setModule('c')
+//         .column(1)
+//           .setModule('d')
+//     .column(6)
+//       .setModule('e');
 
 // grid.render();
