e479b250e4dda24843758c3d185716e5178d073e
Greg Skiano <gregoryskiano@gmail.com>
2014-11-15 00:38:57 -0500

    replacing _partial

--- example/example.js
+++ example/example.js
@@ -21,28 +21,3 @@ grid
       .setModule('e');
 
 grid.render();
-
-// grid.column(9)
-//   .setModule('a')
-//   .setRule(['right','bottom'])
-//   .nest(2)
-//     .column(6)
-//       .nest(2)
-//         .column()
-//           .setModule('smallA')
-//         .column()
-//           .setModule('smallB')
-//     .column(3)
-//       .setModule('c')
-//       .setRule('left')
-//   .nest(3)
-//     .column()
-//       .setModule('d')
-//       .setRule('right')
-//     .column()
-//       .setModule('e')
-//       .setRule('right')
-//     .column()
-//       .setModule('f')
-
-// grid.compile();
