5d1ceec1ce42a39461b60f6c9989a2a0ea9750d2
Greg Skiano <gregoryskiano@gmail.com>
2014-11-16 02:52:40 -0500

    starting to clean it up

--- example/column-example.js
+++ example/column-example.js
@@ -2,21 +2,21 @@
 var COL = require('../lib/COL');
 
 var grid = COL(12)('PAGE')(
-
+  
   COL(6)('HEADER')(
-    
-    COL(4)('MODULE_A')(COL)
+
+    COL(3)('MODULE_A')(COL)
 
   )(
-
-    COL(2)('MODULE_B')(COL)
+    
+    COL(3)('MODULE_B')(COL)
 
   )(COL)
 
 )(
 
-  COL(6)('setBoring')(COL)
+  COL(COL)('setBoring')(COL)
 
 )(COL);
 
-console.log( JSON.stringify(grid,null,2) );
+console.log(JSON.stringify(grid,null,2));
