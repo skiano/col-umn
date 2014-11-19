3983137fa55fc02daf52faa6ab288d8665487c77
Greg Skiano <gregoryskiano@gmail.com>
2014-11-18 20:34:24 -0500

    checking other example

--- example/column-example.js
+++ example/column-example.js
@@ -13,25 +13,25 @@ var test = col(6)(testQueue)(
 console.log(test)
 
 
-// function setContent (options) {
-//   options.content = true;
-// }
+function setContent (options) {
+  options.content = true;
+}
 
-// var build = col(6)
-//     (
-//     col(4)(setContent)
-//     )(
-//     col(2)(setContent)
-//     )(
-//     col(6)(setContent)
-//         (
-//         col(3)(setContent)
-//         )( 
-//         col(3)(setContent) 
-//         )
-//     );
+var build = col(6)
+    (
+    col(4)(setContent)
+    )(
+    col(2)(setContent)
+    )(
+    col(6)(setContent)
+        (
+        col(3)(setContent)
+        )( 
+        col(3)(setContent) 
+        )
+    );
 
-// console.log(JSON.stringify(build(),null,2))
+console.log(JSON.stringify(build(),null,2))
 
 
 
