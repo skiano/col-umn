f0c129f82ab3d866aec1f90cf38ae0566fb9aa30
Greg Skiano <gregoryskiano@gmail.com>
2014-11-18 19:11:04 -0500

    more of an example

--- example/column-example.js
+++ example/column-example.js
@@ -4,21 +4,16 @@ var COL = require('../lib/COL');
 
 var build = COL(6)(
   COL(4)
-)()
+)(
+  COL(2)
+)(
+  COL(6)(
+    COL(3)
+  )(
+    COL(3)
+  )
+)
 
-console.log(build)
-
-
-
-// console.log(build, build2)
-
-
-
-// (
-//   COL(4)
-// )
-
-
-// console.log(JSON.stringify(page,null,2));
+console.log(build())
 
 
