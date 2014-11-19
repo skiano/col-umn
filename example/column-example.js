1830cc4760dce866b0c9875798ae5d2234b8a2d8
Greg Skiano <gregoryskiano@gmail.com>
2014-11-18 21:11:30 -0500

    allowing nesting to work with async

--- example/column-example.js
+++ example/column-example.js
@@ -2,36 +2,49 @@
 var col = require('../lib/col-umn');
 
 function testQueue (column, cb) {
-  console.log('testQueue', column);
-  cb(null);
+
+  console.log('immediate');
+  
+  setTimeout(function () {
+
+    column.awesome = true;
+    console.log('testQueue');
+    cb(null);
+
+  }, 1000);
+
 }
 
-var test = col(6)(testQueue)(
+var c = col(6)(testQueue)(testQueue)(
   col(4)(testQueue)
-)();
+)(true);
 
-console.log(test)
+c(function (column) {
+
+  console.log(JSON.stringify(column,null,2));
+
+});
 
 
-function setContent (options) {
-  options.content = true;
-}
+// function setContent (options) {
+//   options.content = true;
+// }
 
-var build = col(6)
-    (
-    col(4)(setContent)
-    )(
-    col(2)(setContent)
-    )(
-    col(6)(setContent)
-        (
-        col(3)(setContent)
-        )( 
-        col(3)(setContent) 
-        )
-    );
+// var build = col(6)
+//     (
+//     col(4)(setContent)
+//     )(
+//     col(2)(setContent)
+//     )(
+//     col(6)(setContent)
+//         (
+//         col(3)(setContent)
+//         )( 
+//         col(3)(setContent) 
+//         )
+//     );
 
-console.log(JSON.stringify(build(),null,2))
+// console.log(JSON.stringify(build(),null,2))
 
 
 
